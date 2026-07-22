0aabb3df159a57e8b76b26e04bdf119e841fb114
TanjimulSabbir <tanjimulsabbir.dev@gmail.com>
2026-07-23 00:46:20 +0600

    feat: - Added package.json with necessary dependencies and scripts - Created prisma.config.ts for Prisma configuration - Implemented app.ts to set up Express server with middleware and routes - Configured environment variables in config/index.ts - Established Prisma client connection in lib/prisma.ts - Set up server entry point in server.ts - Added utility functions for async error handling (catchAsync.ts) - Implemented JWT utility functions for token creation and verification (jwt.ts) - Developed a response utility for standardized API responses (sendResponse.ts) - Configured TypeScript settings in tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
+import { authRoutes } from "./modules/auth/auth.routes";
+import { commentRoutes } from "./modules/comment/comment.route";
+import { postRoutes } from "./modules/post/post.route";
+import { userRoutes } from "./modules/user/user.route";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.use(notFound);
+
+app.use(globalErrorHandler);
+
+export default app;
