1a36518eadc8ea343fd17f80c589d2d82a1f6456
TanjimulSabbir <tanjimulsabbir.dev@gmail.com>
2026-07-23 18:40:20 +0600

    refactor: simplify app and server setup, update Vercel configuration, and add API entry point

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,7 @@
-import "dotenv/config";
-import app from "./app";
-import config from "./config/index";
-import { prisma } from "./lib/prisma";
+import app from "./app.js";
 
-const PORT = config.port;
+const port = process.env.PORT || 7000;
 
-async function main() {
-  try {
-    await prisma.$connect();
-    console.log("Connected to the database successfully.");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error);
-    await prisma.$disconnect();
-    process.exit(1);
-  }
-}
-
-main();
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
